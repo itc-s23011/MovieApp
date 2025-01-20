932a687fb6db981bbe10bfeb544055f512d53277
itc-s23027 <s23027@std.it-college.ac.jp>
2025-01-20 11:33:31 +0900

    APIの取得確認まで

--- App.js
+++ App.js
@@ -5,33 +5,70 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [movies, setMovies] = useState({});
+  const [nowPlaying, setNowPlaying] = useState({});
+  const [commingSoon, setCommingSoon] = useState({});
+  const [populars, setPopulars] = useState({});
+  const [topRated, setTopRated] = useState({});
+  const [picupMovies, setPicupMovies] = useState({});
 
   useEffect(() => {
     async function getMovies() {
       try {
-        const request = await axios.get(requests.NOW_PLAYING);
-        setMovies(request.data.results);
-        console.log(request.data.results);
+        const nowPlayingMovies = await axios.get(requests.NOW_PLAYING);
+        setNowPlaying(nowPlayingMovies.data.results);
+
+        const commingSoonMovies = await axios.get(requests.COMMING_SOON);
+        setCommingSoon(commingSoonMovies.data.results);
+
+        const popularsMovies = await axios.get(requests.POPULARS);
+        setPopulars(popularsMovies.data.results);
+
+        const topRatedMovies = await axios.get(requests.TOP_RATED);
+        setTopRated(topRatedMovies.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    async function getPickUpMovie() {
+      try {
+        const result = await axios.get(requests.NOW_PLAYING);
+        const number = Math.floor(Math.random() * (result.data.results.length - 1) + 1);
+        setPicupMovies(result.data.results[number]);
       } catch (error) {
         console.log(error);
       }
     }
     getMovies();
+    getPickUpMovie();
   }, []);
- 
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.pickupContainer}>
-        <View style={styles.pickupImage}></View>
-        <Text style={styles.pickupTitle}>映画のタイトル</Text>
+        <Image style={styles.pickupImage} source={{uri: `https://image.tmdb.org/t/p/w780${picupMovies.poster_path}`}}></Image>
+        <Text style={styles.pickupTitle}>{picupMovies.title}</Text>
       </View>
 
       <Text style={styles.listName}>公開中の映画</Text>
 
 
       <FlatList
-        data={movies}
+        data={nowPlaying}
+        keyExtractor={item => item.id}
+        horizontal={true}
+        flashScrollIndicators
+        renderItem={({ item }) => (
+        <View style={styles.movieContainer}>
+          <Image style={styles.movieImage} source={{uri: `https://image.tmdb.org/t/p/w300${item.poster_path}`}}></Image>
+          <Text numberOfLines={1} style={styles.movieTitle}>{item.title}</Text>
+        </View>
+        )}>
+      </FlatList>
+      <Text style={styles.listName}>公開予定の映画</Text>
+
+
+      <FlatList
+        data={commingSoon}
         keyExtractor={item => item.id}
         horizontal={true}
         flashScrollIndicators
@@ -43,72 +80,35 @@ export default function App() {
         )}>
       </FlatList>
 
-
-      {/* <View style={{backgroundColor: 'blue', width: '100%', flexDirection: 'row'}}>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-      </View> */}
-
-      <Text style={styles.listName}>公開予定の映画</Text>
-
-      <View style={{backgroundColor: 'blue', width: '100%', flexDirection: 'row'}}>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-      </View>
-
       <Text style={styles.listName}>人気の映画</Text>
 
-      <View style={{backgroundColor: 'blue', width: '100%', flexDirection: 'row'}}>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
+      <FlatList
+        data={populars}
+        keyExtractor={item => item.id}
+        horizontal={true}
+        flashScrollIndicators
+        renderItem={({ item }) => (
+        <View style={styles.movieContainer}>
+          <Image style={styles.movieImage} source={{uri: `https://image.tmdb.org/t/p/w300${item.poster_path}`}}></Image>
+          <Text numberOfLines={1} style={styles.movieTitle}>{item.title}</Text>
         </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-      </View>
+        )}>
+      </FlatList>
 
       <Text style={styles.listName}>高評価の映画</Text>
 
-      <View style={{backgroundColor: 'blue', width: '100%', flexDirection: 'row'}}>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
+      <FlatList
+        data={topRated}
+        keyExtractor={item => item.id}
+        horizontal={true}
+        flashScrollIndicators
+        renderItem={({ item }) => (
+        <View style={styles.movieContainer}>
+          <Image style={styles.movieImage} source={{uri: `https://image.tmdb.org/t/p/w300${item.poster_path}`}}></Image>
+          <Text numberOfLines={1} style={styles.movieTitle}>{item.title}</Text>
         </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-        <View>
-          <View style={styles.movieImage}></View>
-          <Text style={styles.movieTitle}>映画のタイトル</Text>
-        </View>
-      </View>
+        )}>
+      </FlatList>
       <StatusBar style="auto" />
     </ScrollView>
   );
@@ -120,16 +120,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#202328'
   },
   pickupContainer: {
-    backgroundColor: 'red',
+
     width: '100%', 
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: 'row', 
+    alignItems: 'center', 
     justifyContent: 'center'
   },
   pickupImage: {
-    backgroundColor: 'gray',
+    
     height: 350, 
-    width: '45%'
+    width: '45%',
+    resizeMode: 'contain'
   },
   pickupTitle: {
     color: '#fff', 
@@ -141,11 +142,11 @@ const styles = StyleSheet.create({
   listName: {
     color: '#fff', 
     fontSize: 18, 
-    fontWeight: 'bold',
-    marginBottom: 5
+    fontWeight: 'bold'
   },
   movieContainer: {
-    width: 130
+    width: 130,
+    marginBottom:30
   },
   movieImage: {
     height: 200,
